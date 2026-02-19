91291d8d27b8a039756876e6c9e9b40179c6e241
Danna0327 <155197785+Danna0327@users.noreply.github.com>
2026-02-18 20:21:14 -0500

    Add route to get active categories

--- routes/curso.routes.js
+++ routes/curso.routes.js
@@ -9,6 +9,7 @@ router.post("/crear",                               cursoController.crearCurso);
 router.put("/:cursoId",                             cursoController.editarCurso);
 router.get("/docente",                              cursoController.obtenerCursosDocente);
 router.get("/estudiantes",                          cursoController.obtenerEstudiantesDocente);
+router.get("/categorias-activas",                   cursoController.obtenerCategoriasActivas);
 router.get("/:cursoId/analytics",                   cursoController.obtenerAnalyticsCurso);
 router.delete("/:cursoId/estudiante/:estudianteId", cursoController.eliminarEstudianteCurso);
 
